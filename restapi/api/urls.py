030a4c8b9e41204cd35a569b87f1a5d344d31cb1
Esther Wanjiru  Kungu <93114558+wanj1ru@users.noreply.github.com>
2024-01-24 15:31:18 +0300

    Update urls.py

--- restapi/api/urls.py
+++ restapi/api/urls.py
@@ -1,2 +1,11 @@
-[InternetShortcut]
-URL=https://github.com/apogiatzis/restful-user-api-django-drf/blob/master/api/urls.py
+from django.conf.urls import url, include
+from rest_framework import routers
+from api.views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^auth/', include('rest_auth.urls')),
+]
