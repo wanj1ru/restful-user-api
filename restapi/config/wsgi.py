4f588e424f255e83f41a339a375c7a8eea5b3954
Esther Wanjiru  Kungu <93114558+wanj1ru@users.noreply.github.com>
2024-01-24 15:33:58 +0300

    Update wsgi.py

--- restapi/config/wsgi.py
+++ restapi/config/wsgi.py
@@ -1,2 +1,16 @@
-[InternetShortcut]
-URL=https://github.com/apogiatzis/restful-user-api-django-drf/blob/master/config/wsgi.py
+"""
+WSGI config for config project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = get_wsgi_application()
