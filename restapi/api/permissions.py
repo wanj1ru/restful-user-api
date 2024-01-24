b5b1e8c1c6817e9c692799fbb4aad5dd22f7a286
Esther Wanjiru  Kungu <93114558+wanj1ru@users.noreply.github.com>
2024-01-24 15:28:09 +0300

    Update permissions.py

--- restapi/api/permissions.py
+++ restapi/api/permissions.py
@@ -1,2 +1,16 @@
-[InternetShortcut]
-URL=https://github.com/apogiatzis/restful-user-api-django-drf/blob/master/api/permissions.py
+from rest_framework import permissions
+
+
+class IsLoggedInUserOrAdmin(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user or request.user.is_staff
+
+
+class IsAdminUser(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        return request.user and request.user.is_staff
